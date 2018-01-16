34b6717af8ee2fd7ec01f2252db28817cba7532d
Joey Kleingers <joey.kleingers@bluequartz.net>
2018-01-16 09:43:37 -0500

    Add the override C++11 keyword to all methods that are overridden from AbstractFilter.  Replaced macro SIMPL_TYPE_MACRO_SUPER with SIMPL_TYPE_MACRO_SUPER_OVERRIDE to get the override keyword where needed.

--- SMTKPluginFilters/ExportMoabMesh.h
+++ SMTKPluginFilters/ExportMoabMesh.h
@@ -45,7 +45,7 @@ class ExportMoabMesh : public AbstractFilter
   public:
     SIMPL_SHARED_POINTERS(ExportMoabMesh)
     SIMPL_STATIC_NEW_MACRO(ExportMoabMesh)
-    SIMPL_TYPE_MACRO_SUPER(ExportMoabMesh, AbstractFilter)
+    SIMPL_TYPE_MACRO_SUPER_OVERRIDE(ExportMoabMesh, AbstractFilter)
 
     virtual ~ExportMoabMesh();
 
@@ -58,56 +58,56 @@ class ExportMoabMesh : public AbstractFilter
     /**
      * @brief getCompiledLibraryName Reimplemented from @see AbstractFilter class
      */
-    virtual const QString getCompiledLibraryName();
+    virtual const QString getCompiledLibraryName() override;
 
     /**
      * @brief getBrandingString Returns the branding string for the filter, which is a tag
      * used to denote the filter's association with specific plugins
      * @return Branding string
     */
-    virtual const QString getBrandingString();
+    virtual const QString getBrandingString() override;
 
     /**
      * @brief getFilterVersion Returns a version string for this filter. Default
      * value is an empty string.
      * @return
      */
-    virtual const QString getFilterVersion();
+    virtual const QString getFilterVersion() override;
 
     /**
      * @brief newFilterInstance Reimplemented from @see AbstractFilter class
      */
-    virtual AbstractFilter::Pointer newFilterInstance(bool copyFilterParameters);
+    virtual AbstractFilter::Pointer newFilterInstance(bool copyFilterParameters) override;
 
     /**
      * @brief getGroupName Reimplemented from @see AbstractFilter class
      */
-    virtual const QString getGroupName();
+    virtual const QString getGroupName() override;
 
     /**
      * @brief getSubGroupName Reimplemented from @see AbstractFilter class
      */
-    virtual const QString getSubGroupName();
+    virtual const QString getSubGroupName() override;
 
     /**
      * @brief getHumanLabel Reimplemented from @see AbstractFilter class
      */
-    virtual const QString getHumanLabel();
+    virtual const QString getHumanLabel() override;
 
     /**
      * @brief setupFilterParameters Reimplemented from @see AbstractFilter class
      */
-    virtual void setupFilterParameters();
+    virtual void setupFilterParameters() override;
 
     /**
      * @brief execute Reimplemented from @see AbstractFilter class
      */
-    virtual void execute();
+    virtual void execute() override;
 
     /**
     * @brief preflight Reimplemented from @see AbstractFilter class
     */
-    virtual void preflight();
+    virtual void preflight() override;
 
   signals:
     /**
